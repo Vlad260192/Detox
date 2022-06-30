5966134eabc9bb76854ee4558d16838a9ac5d081
Vlad260192 <v.kapustianskiy@gmail.com>
2022-06-30 15:01:26 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
+// Burger
 $(document).ready(function() {
     $('.header__burger').click(function(event) {
         $('.header__burger,.header__menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
+});
+
+// Slick-slider
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:false,
+		slidesToShow:1,
+		autoplay:true,
+		speed:1500,
+		autoplaySpeed:800,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:1
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
 });
